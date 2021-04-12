bf9845c177dd83aaeb84143c5e654a0a7565bc11
Anıl Mısırlıoğlu <misirlioglisamet@gmail.com>
2021-04-13 00:13:43 +0300

    feat(http-server): cli mode implemented
    Signed-off-by: Anıl Mısırlıoğlu <misirlioglisamet@gmail.com>

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"os"
 )
 
 var (
@@ -11,6 +15,7 @@ var (
 	Token     string
 	DBLToken  string
 	SheetID   string
+	isCLI     bool
 )
 
 type Stats struct {
@@ -33,6 +38,7 @@ func init() {
 	flag.StringVar(&Token, "webhook_token", "", "Webhook API Token")
 	flag.StringVar(&DBLToken, "dbl_token", "", "TopGG API Token")
 	flag.StringVar(&SheetID, "sheet_id", "", "Google Spreadsheet ID")
+	flag.BoolVar(&isCLI, "cli", false, "Run as a one of tool")
 	flag.Parse()
 }
 
@@ -41,10 +47,36 @@ func main() {
 		log.Fatal("missing parameters")
 	}
 
-	err := run()
-	if err != nil {
-		log.Fatalf("%s", err)
+	if isCLI {
+		if err := run(); err != nil {
+			log.Fatalf("%s", err)
+		}
+
+		return
 	}
+
+	serveHTTP()
+}
+
+func serveHTTP() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT environment variable required if not running as --cli")
+	}
+
+	http.HandleFunc("/health", func(_ http.ResponseWriter, _ *http.Request) {})
+	http.HandleFunc("/run", func(rw http.ResponseWriter, _ *http.Request) {
+		if err := run(); err != nil {
+			rw.WriteHeader(http.StatusInternalServerError)
+			_, _ = fmt.Fprintf(io.MultiWriter(os.Stderr, rw), "error: %v\n", err)
+			return
+		}
+
+		_, _ = fmt.Fprintf(rw, "ok")
+	})
+
+	log.Printf("server starting at: %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
 func run() error {
